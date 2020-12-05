953407097214f53d59091cafc6386782ee7aa59e
kiranpranay <kiranpranay12@gmail.com>
2020-12-05 15:04:18 +0530

    typed fix

--- main-js/typed.js
+++ main-js/typed.js
@@ -1,6 +1,6 @@
 $(function () {
   $(".typed").typed({
-    strings: ["STM.", "NGO.", "SETT."],
+    strings: ["Donate Oraphanages.", "Donate Oldage Homes.", "Are With You."],
     // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
     stringsElement: null,
     // typing speed
