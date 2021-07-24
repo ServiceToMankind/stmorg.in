5ee2b5598576216577d85d7a493c7dcdd2aa08a2
kiranpranay <kiranpranay12@gmail.com>
2021-07-25 01:06:32 +0530

    connection update

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 // session started here
 session_start();
-$con = mysqli_connect("35.224.143.203","common","common@2001@common","stm");
+$con = mysqli_connect("34.136.3.209","master","cotton2001","stm");
 if(mysqli_connect_errno()) {  
     die("Failed to connect with MySQL: ". mysqli_connect_error());  
 }
