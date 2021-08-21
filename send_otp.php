833a314a65202d18706d847b3496b8890599b773
kiranpranay <pranaybannu12@gmail.com>
2021-08-21 13:40:31 +0530

    STM : Send OTP from custom API
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- send_otp.php
+++ send_otp.php
@@ -19,37 +19,15 @@ die();
 
 
     // include('smtp/PHPMailerAutoload.php');
-include('smtp/PHPMailerAutoload.php');
-$html="hey, $otp is your otp!";
-echo smtp_mailer('user_email_id','OTP MASTER',$html);
-function smtp_mailer($to,$subject, $msg){
-	$mail = new PHPMailer(); 
-	$mail->SMTPDebug  = 3;
-	$mail->IsSMTP(); 
-	$mail->SMTPAuth = true; 
-	$mail->SMTPSecure = 'tls'; 
-	$mail->Host = "smtp.gmail.com";
-	$mail->Port = 587; 
-	$mail->IsHTML(true);
-	$mail->CharSet = 'UTF-8';
-	$mail->Username = "otp.stm@gmail.com";
-	$mail->Password = "otpmankind004";
-	$mail->SetFrom("otp.stm@gmail.com");
-	$mail->Subject = $subject;
-	$mail->Body =$msg;
-	$mail->AddAddress($to);
-	$mail->SMTPOptions=array('ssl'=>array(
-		'verify_peer'=>false,
-		'verify_peer_name'=>false,
-		'allow_self_signed'=>false
-	));
-	if(!$mail->Send()){
-		echo $mail->ErrorInfo;
-	}else{
-		return 'Sent';
-	}
-}
-
+// include('smtp/PHPMailerAutoload.php');
+	$url='http://cottonmail.rf.gd/mailer.php?html='.$html.'&to='.$to;
+	$ch=curl_init();
+	curl_setopt($ch,CURLOPT_URL,$url);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+	$result=curl_exec($ch);
+	curl_close($ch);
+	$result=json_decode($result,true);
+	echo $return=$result['return'];
 
 }
 
