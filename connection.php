a91e7968e90eb707fa12ee0405062b0050964fe6
kiranpranay <pranaybannu12@gmail.com>
2022-01-25 19:04:32 +0530

    add .gitignore
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 // session started here
 session_start();
-$con = mysqli_connect("34.136.3.209","master","cotton2001","stm");
+$con = mysqli_connect("localhost","root","","stm");
 if(mysqli_connect_errno()) {  
     die("Failed to connect with MySQL: ". mysqli_connect_error());  
 }
