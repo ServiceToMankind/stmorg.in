917940fbeef49c4cb70867e1d1df5cac59d3b7ef
kiranpranay <pranaybannu12@gmail.com>
2022-06-24 12:02:29 +0530

    STM: Update phpmailer from composer
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f882b39866231b0ea8ed8b6a68469fc25d2ab9ad',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f882b39866231b0ea8ed8b6a68469fc25d2ab9ad',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v5.2.28',
+            'version' => '5.2.28.0',
+            'reference' => 'acba50393dd03da69a50226c139722af8b153b11',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
