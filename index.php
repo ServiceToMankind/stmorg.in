3ff64705840ea10eaf4609edf31ffd41fdb7a84f
kiranpranay <pranaybannu12@gmail.com>
2022-07-07 19:36:24 +0530

    STM: add #askstm
    * #ASKSTM is a platform, where you can get anytype of medical help from STM.
    It includes blood,platelets,medical funds,..etc
    
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ $day= date('d',$dateValue); ?>
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
                         <a class="nav-link" style="color:red !important;
-                        " href="https://covid.stmorg.in">COVID</a>
+                        " href="https://ask.stmorg.in">#ASKSTM</a>
                     </li>
                     <li class="nav-item active">
                         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
