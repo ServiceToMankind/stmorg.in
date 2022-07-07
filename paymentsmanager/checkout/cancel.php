ac7dd36c41aa3b12bdc1a37252cff550157471a6
kiranpranay <pranaybannu12@gmail.com>
2022-07-07 23:32:27 +0530

    STM: BringUp cancel page
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

---
+++ paymentsmanager/checkout/cancel.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- fix iOS 10~ bug -->
+    <meta name=“x-apple-disable-message-reformatting”>
+    <!-- fix IE bug -->
+    <meta http-equiv=“X-UA-Compatible” content=“IE=edge,chrome=1”>
+    <title>Payment Failed</title>
+</head>
+
+<body style="width:100%; max-width: 660px; min-width: 300px; margin: 0 auto;">
+
+    <span style="color:transparent; display:none; height:0; opacity:0; visibility:hidden; width:0">We tried to process
+        your payment but had no luck. Please take a look at your billing information.</span>
+
+    <table align="center" style="padding:30px;margin-top:10px;" width="100%">
+        <tbody>
+            <tr>
+                <td>
+                    <img src="accesories\service_to_man_kind-20200709-0001.jpg" width="125px"
+                        style="margin-bottom:10px; margin-left:-6px;">
+                </td>
+            </tr>
+            <tr>
+                <td
+                    style="padding-right:30px; padding-left:30px; padding-top:50px; padding-bottom:50px; color:#717171; font-family:arial; font-size:14px; border: 0px;border-radius:8px;box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.1);">
+                    <span style="font-size:25px;"><b>Uh oh, your payment failed :(</b></span>
+                    <br><br><br>We tried to process your payment but had no luck.
+                    <br><br>We suggest you review your billing information and make changes if necessary. If you’re sure
+                    everything’s correct, email us to let us know.
+
+                    <br><br><br><br>
+                    <center><a href="https://stmorg.in/donate"
+                            style="background-color:#534ce8; padding-left:30px; padding-right:30px; padding-top:15px; padding-bottom:15px; text-decoration: none; border-radius:5px;font-size:13px; color:#fff;">Try
+                            Again !</a></center>
+                    <br><br>
+                    <center><span style="font-size:12px;">Have any questions? Please write to <a
+                                href="mailto:servicetomankindorganization@gmail.com"
+                                style="color:#81CDFF;">servicetomankindorganization@gmail.com</a> and we’d be happy to
+                            answer
+                            them.</span></center>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    <table align="center" width="100%">
+        <tbody>
+            <tr>
+                <td align="center" style="font-family:arial; font-size:11px; color:#717171;">
+                    <span>&copy; 2022 <a href="https://stmorg.in">stmorg.in</a> All rights reserved</span><br><br><br>
+                    <a href="https://facebook.com/stmorg.in"><img
+                            src="https://resources.mlytics.com/hubfs/Social%20Icons/iconmonstr-facebook-4-32.png"></a>&nbsp;&nbsp;
+                    <a href="https://stmorg.in/twitter.com/STM_ORG"><img
+                            src="https://resources.mlytics.com/hubfs/Social%20Icons/iconmonstr-twitter-4-32.png"></a>&nbsp;&nbsp;
+                    <a href="https://www.linkedin.com/company/servicetomankind/"><img
+                            src="https://resources.mlytics.com/hubfs/Social%20Icons/iconmonstr-linkedin-4-32.png"></a>&nbsp;&nbsp;
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+
+</html>
