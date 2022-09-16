1b4c7ecc20e7910a36609d1d35c5f968c3d3af21
kiranpranay <kiranpranay12@gmail.com>
2022-09-16 16:06:18 +0000

    temp:server #2

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
-<?php
+\<?php
 // session started here
 session_start();
-$con = mysqli_connect("localhost","root","","stm");
+$con = mysqli_connect("34.172.217.6","hydro","hydro123","stm");
 if(mysqli_connect_errno()) {  
     die("Failed to connect with MySQL: ". mysqli_connect_error());  
 }
