94d5fee9317e06360d315c4d68f9bb01a5ef4787
kiranpranay <pranaybannu12@gmail.com>
2023-03-11 21:46:28 +0530

    stm: use predefined functions to fetch donations
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- functions.php
+++ functions.php
@@ -83,9 +83,35 @@ function get_total_price_by_month($con,$date){
     }
     return $raised;
 }
+function get_total_price_by_date($con,$date){
+  $raised=0;
+      $resp=mysqli_query($con,"SELECT `donations`.`amount` from `donations` where `donations`.`payment_status`='1' and `donations`.`added_on` = '$date' and year(`added_on`)=year('$date')");
+    while($row=mysqli_fetch_assoc($resp)){
+       $raised=$raised+$row['amount'];
+    }
+    return $raised;
+}
 function get_total_spends_by_month($con,$date){
   $raised=0;
       $resp=mysqli_query($con,"SELECT `spends`.`amount` from `spends` where month(`spends`.`date`)=month('$date') and year(`date`)=year('$date')");
+    while($row=mysqli_fetch_assoc($resp)){
+       $raised=$raised+$row['amount'];
+    }
+    return $raised;
+}
+// by year 
+function get_total_price_by_year($con,$date){
+  $raised=0;
+      $resp=mysqli_query($con,"SELECT `donations`.`amount` from `donations` where `donations`.`payment_status`='1' and year(`added_on`)=year('$date')");
+    while($row=mysqli_fetch_assoc($resp)){
+       $raised=$raised+$row['amount'];
+    }
+    return $raised;
+}
+// total donations 
+function get_total_donations($con){
+  $raised=0;
+      $resp=mysqli_query($con,"SELECT `donations`.`amount` from `donations` where `donations`.`payment_status`='1'");
     while($row=mysqli_fetch_assoc($resp)){
        $raised=$raised+$row['amount'];
     }
