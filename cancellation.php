e1a325d38b0f3c947963389e2b310a78f1259d28
kiranpranay <pranaybannu12@gmail.com>
2023-03-20 21:28:35 +0530

    STM: BringUp Policy Pages
    * terms of service
    * Pricay Policy
    * Cancellation
    
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

---
+++ cancellation.php
@@ -0,0 +1,64 @@
+<?php
+
+// include the header
+include 'header.php';
+?>
+
+<main style="margin-top: 5em;">
+    <div style="margin: 5em 10em 0em 10em; width:50em">
+        <!-- add this content  -->
+        <!--
+            Cancellation and Refund Policy:
+
+Service to Mankind (STM) NGO acknowledges the importance of donations and values the trust placed in us by donors. We have a transparent policy regarding cancellations and refunds for donations made on our website.
+
+Cancellation Policy:
+
+If the donor wishes to cancel their donation, they can do so within 24 hours of making the donation. After 24 hours, we will not accept any cancellation requests.
+
+Refund Policy:
+
+STM assures the donors that their donations will be utilized solely for the welfare and betterment of society. However, in case the donor is not satisfied with our service or if the donation has not been used for the intended purpose, we will initiate a refund.
+
+Refund Procedure:
+
+The donor can request a refund by writing to us at reach@stmorg.in within 7 days of making the donation. We will investigate the matter and initiate the refund if the claim is found to be genuine.
+
+Note: The refund process may take up to 14 days to complete.
+
+Contact Us:
+
+If you have any questions or concerns about our Cancellation and Refund Policy, please reach out to us at reach@stmorg.in. We will be happy to assist you.
+
+         -->
+        <h1>Cancellation and Refund Policy</h1>
+        <p>Service to Mankind (STM) NGO acknowledges the importance of donations and values the trust placed in us by
+            donors. We have a transparent policy regarding cancellations and refunds for donations made on our website.
+        </p>
+        <h5>Cancellation Policy:</h5>
+        <p>If the donor wishes to cancel their donation, they can do so within 24 hours of making the donation. After 24
+            hours, we will not accept any cancellation requests. </p>
+        <h5>Refund Policy:</h5>
+        <p>STM assures the donors that their donations will be utilized solely for the welfare and betterment of
+            society.
+            However, in case the donor is not satisfied with our service or if the donation has not been used for the
+            intended purpose, we will initiate a refund.</p>
+        <h5>Refund Procedure:</h5>
+        <p>The donor can request a refund by writing to us at <a href="mailto:reach@stmorg.in">reach@stmorg.in</a>
+            within 7
+            days of making the donation. We will investigate the matter and initiate the refund if the claim is found to
+            be genuine.</p>
+        <p><b>Note:</b> The refund process may take up to 14 days to complete.</p>
+        <h5>Contact Us:</h5>
+        <p>If you have any questions or concerns about our Cancellation and Refund Policy, please reach out to us at <a
+                href="mailto:reach@stmorg.in">reach@stmorg.in</a>. We will be happy to assist you.</p>
+
+
+    </div>
+
+    <?php
+include 'footer.php';
+?>
+
+</main>
+</body>
