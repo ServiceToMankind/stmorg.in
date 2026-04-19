9166780f4cfae39457a23c07bf3154f0b77c730c
kiranpranay <pranay@cotonseeds.org>
2026-04-19 23:09:22 +0530

    fix: Update api url
    Signed-off-by: kiranpranay <pranay@cotonseeds.org>

--- includes/functions.php
+++ includes/functions.php
@@ -1,36 +1,41 @@
 <?php
-$api_url = 'http://localhost/apis-stm';
+$api_url = 'https://apis.stmorg.in';
 
-function sanitize_input($input) {
-  return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+function sanitize_input($input)
+{
+    return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
 }
 
-function pr($arr){
-    echo'<pre>';
+function pr($arr)
+{
+    echo '<pre>';
     print_r($arr);
 }
 
-function prx($arr){
-    echo'<pre>';
+function prx($arr)
+{
+    echo '<pre>';
     print_r($arr);
     die();
 }
 
-function get_api_data($url){
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  $output = curl_exec($ch);
-  curl_close($ch);
-  return $output;
+function get_api_data($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
 }
 
-function get_api_data_post($url,$data){
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  $output = curl_exec($ch);
-  curl_close($ch);
-  return $output;
+function get_api_data_post($url, $data)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
 }
